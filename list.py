98fde618506ef0c5bc6fa0be859ba1280a5094a6
salihabrik <briksaliha@gmail.com>
2023-06-02 02:14:39 +0200

    hello

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-l1 =[10,20,30,40,50]
+"""l1 =[10,20,30,40,50]
 l2 = ["mohamed",35,"ali",40]
 l3 = list()
 l4 = ([12,14,16])
@@ -19,3 +19,20 @@ l1.reverse()
 print(l1)
 l1.remove(20)
 print(l1)
+"""
+t1 = (10,20,30,40,50,80,90 )
+t2 = tuple ([50,60,70,80])
+t3 = tuple()
+t4 = tuple("puthon")
+print(t1)
+print(t1[0])
+print(t1[2:])
+print(t1[:4])
+print(t1[2:])
+print(max(t2),min(t1),len(t1),sum(t1))
+print(60 in t2)
+t7 = ((1,2,3),(4,5,6),"hello")
+print(t7[0])
+print(t7[0][2])
+for i in t1:
+    print(i ,end="\n")
