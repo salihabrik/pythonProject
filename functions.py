c02cd741d1a7e9873538de29dd78c80a6a7b6027
salihabrik <briksaliha@gmail.com>
2023-06-02 04:07:48 +0200

    no one

--- functions.py
+++ functions.py
@@ -1,5 +1,9 @@
-def sum(start,end):
+"""def sum(start,end):
     result =0
+    if (start>end):
+        print("end must be greater than staet")
+        return
+
     for i in range(start,end):
         result = result+i
     return result
@@ -7,3 +11,12 @@ s= int(input("enter start num"))
 p =int(input("enter end num"))
 output = sum(s,p)
 print(output)
+"""
+#############################
+def bigger(a,b):
+    if a>b:
+        return a,b
+    else:
+        return b,a
+result = bigger(10,30)
+print (result,type(result))
