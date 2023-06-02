b924e088e2e044477a872e0c2ee7458bbed34fb1
salihabrik <briksaliha@gmail.com>
2023-06-02 02:39:16 +0200

    hi

---
+++ count.py
@@ -0,0 +1,13 @@
+
+countries ={}
+countries ["Algeria"] =100
+countries ["KSA"] = 25
+countries ["USA"]= 200
+print(countries)
+countries ["Algeria"]= 333
+print(countries)
+print(len(countries))
+print("tunis" in countries)
+d1 = {"mike" :33,"bob":44,"hih":22}
+d2 = {"mike" :33,"bob":44,"hih":22}
+print (d1 == d2)
