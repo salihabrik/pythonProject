4f9bcc2198f203a3592facefca2dff4a15d011b1
salihabrik <briksaliha@gmail.com>
2023-06-02 04:09:17 +0200

    no one

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-"""def sum(start,end):
+def sum(start,end):
     result =0
     if (start>end):
         print("end must be greater than staet")
@@ -11,7 +11,7 @@ s= int(input("enter start num"))
 p =int(input("enter end num"))
 output = sum(s,p)
 print(output)
-"""
+
 #############################
 def bigger(a,b):
     if a>b:
