b99d1ea7b4906f4b8e47707805435f18885f2f29
salihabrik <briksaliha@gmail.com>
2024-02-22 22:41:36 +0100

    send email

--- pyhon-automation/oop.py
+++ pyhon-automation/oop.py
@@ -1,24 +1,23 @@
+class Students:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
 
-        
-    class Students :
-            def __init__(self, name, score):
-                self.name = name
-                self.score = score
-        class course:
-            def __init__  (self, course_name, max_score) 
-                self.course_name = course_name
-                self.max_score = max_score 
-                self.students = [] 
-                
-            def add_students(self, student) :
-                if student.score >= self.max_score:
-                    self.students.append(student)
-                
+class Course:
+    def __init__(self, course_name, max_score):
+        self.course_name = course_name
+        self.max_score = max_score
+        self.students = []
 
-s1 = Students("mohamed" , 88)
-s2 = Students("Khaled ", 66)
-s3 = students("joe",77)
+    def add_students(self, student):
+        if student.score >= self.max_score:
+            self.students.append(student)
 
-c2 = course("ethical_hacking", 90)
+s1 = Students("mohamed", 88)
+s2 = Students("Khaled", 66)
+s3 = Students("joe", 77)
+
+c2 = Course("ethical_hacking", 90)
 c2.add_students(s2)
-print(c2.students[0])
+c2.add_students(s1)
+print(c2.students[0].name)
