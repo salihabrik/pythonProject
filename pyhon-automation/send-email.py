b2505cc1b5f624168d9acf4c2ab5670c215aa821
salihabrik <briksaliha@gmail.com>
2024-02-22 18:58:59 +0100

    automation

---
+++ pyhon-automation/send-email.py
@@ -0,0 +1,26 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Set up the message
+subject = "Google Meeting"
+body = "Hello,\nDo you want to do a Google Meet"
+message = MIMEMultipart()
+message["From"] = "@gmail.com"
+message["To"] = "@gmail.com"
+message["Subject"] = subject
+
+message.attach(MIMEText(body, "plain"))
+
+# Connect to the SMTP server
+with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    server.ehlo()
+    server.starttls()
+
+    # Login to your Gmail account
+    server.login("email@gmail.com", "password")
+
+    # Send the email
+    server.sendmail("email@gmail.com", "email@gmail.com", message.as_string())
+
+print("Email sent successfully.")
