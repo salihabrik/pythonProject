12302a13d43ed95ae0535141c054abe870bc9091
salihabrik <briksaliha@gmail.com>
2024-02-29 17:17:04 +0100

    update

--- pyhon-automation/hangman.py
+++ pyhon-automation/hangman.py
@@ -15,8 +15,18 @@ guess = input("Guess a letter ").lower()
 
 #print(guess)
 
-for letter in secret_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+#for letter in secret_word:
+ #   if letter == guess:
+  #      print("Right")
+   # else:
+    #    print("Wrong")
+    
+game_over = false
+
+while not game_over:
+    
+    for position in range(len(secret_word)):
+        letter = secret_word[position]
+        if letter == guess:
+            display_word[position] = letter
+    print(display_word))
