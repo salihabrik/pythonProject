dcd639dcbe63c3ba5836745797c1d9bfded3c403
salihabrik <briksaliha@gmail.com>
2024-02-29 17:49:20 +0100

    updtae

--- pyhon-automation/hangman.py
+++ pyhon-automation/hangman.py
@@ -4,13 +4,12 @@ print ("Welcome to Hangman Game")
 words = ['hacker', 'bounty', 'random']
 
 secret_word = random.choice(words)
-
+print ("secret_word")
+print("you get 5 guesses")
 display_word = []
 for letter in secret_word:
     display_word += "_"
-    print(display_word)
-    
-    
+    print(display_word)    
 guess = input("Guess a letter ").lower()
 
 #print(guess)
@@ -20,13 +19,24 @@ guess = input("Guess a letter ").lower()
   #      print("Right")
    # else:
     #    print("Wrong")
-    
-game_over = false
+num = 0   
+game_over = False
 
 while not game_over:
-    
+    guess = input("Guess a letter ").lower()
     for position in range(len(secret_word)):
         letter = secret_word[position]
         if letter == guess:
             display_word[position] = letter
-    print(display_word))
+            if guess not in secret_word:
+                num += 1
+                guesses_left = 5 - num
+                print(f" you have {guesses_left} guesses left ")
+                if num >= 5:
+                    print("you loser") 
+                    game_over = True
+    print(display_word)
+    
+    if "_" not in display_word:
+        print("you win")
+        game_over = True
